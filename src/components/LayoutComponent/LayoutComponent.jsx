760cc7d02b193b2e37a8492c1cab24aaefd9b1c0
tni <tni_consultant@mail.ru>
2022-11-03 22:44:47 +0300

    les3

--- src/components/LayoutComponent/LayoutComponent.jsx
+++ src/components/LayoutComponent/LayoutComponent.jsx
@@ -1,5 +1,15 @@
 import style from './LayoutComponent.module.css';
+import PropTypes from 'prop-types';
 
 export const LayoutComponent = ({ children }) => {
-  return <div className={style.container}>{children}</div>
-}
+  return <div className={style.container}>{children}</div>;
+};
+
+LayoutComponent.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.array,
+    PropTypes.node
+  ]),
+};
